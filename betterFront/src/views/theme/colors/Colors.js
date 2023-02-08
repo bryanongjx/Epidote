b8d0efa032f8e7750628063c279e499439489242
bryanongjxairetec <92636396+bryanongjx@users.noreply.github.com>
2023-02-08 22:08:08 +0800

    added health and about us page

--- betterFront/src/views/theme/colors/Colors.js
+++ betterFront/src/views/theme/colors/Colors.js
@@ -1,66 +1,67 @@
-import PropTypes from 'prop-types'
-import React, { useEffect, useState, createRef } from 'react'
-import classNames from 'classnames'
-import { CRow, CCol, CCard, CCardHeader, CCardBody } from '@coreui/react'
-import { rgbToHex } from '@coreui/utils'
+import React from 'react'
+import { CCard, CCardHeader, CCardBody } from '@coreui/react'
 import { DocsLink } from 'src/components'
-
-const ThemeView = () => {
-  const [color, setColor] = useState('rgb(255, 255, 255)')
-  const ref = createRef()
-
-  useEffect(() => {
-    const el = ref.current.parentNode.firstChild
-    const varColor = window.getComputedStyle(el).getPropertyValue('background-color')
-    setColor(varColor)
-  }, [ref])
-
-  return (
-    <table className="table w-100" ref={ref}>
-      <tbody>
-        <tr>
-          <td className="text-medium-emphasis">HEX:</td>
-          <td className="font-weight-bold">{rgbToHex(color)}</td>
-        </tr>
-        <tr>
-          <td className="text-medium-emphasis">RGB:</td>
-          <td className="font-weight-bold">{color}</td>
-        </tr>
-      </tbody>
-    </table>
-  )
-}
-
-const ThemeColor = ({ className, children }) => {
-  const classes = classNames(className, 'theme-color w-75 rounded mb-3')
-  return (
-    <CCol xs={12} sm={6} md={4} xl={2} className="mb-4">
-      <div className={classes} style={{ paddingTop: '75%' }}></div>
-      {children}
-      <ThemeView />
-    </CCol>
-  )
-}
-
-ThemeColor.propTypes = {
-  children: PropTypes.node,
-  className: PropTypes.string,
-}
 
 const Colors = () => {
   return (
     <>
       <CCard className="mb-4">
         <CCardHeader>
-          My Health
-          <DocsLink href="https://coreui.io/docs/utilities/colors/" />
+          Profile
+          <DocsLink href="https://coreui.io/docs/content/typography/" />
         </CCardHeader>
         <CCardBody>
-          <CRow>
-            <ThemeColor className="bg-primary">
-              <h6>Brand Primary Color</h6>
-            </ThemeColor>
-          </CRow>
+          <p>The details of the user is given.</p>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Information</th>
+                <th>Description</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <p>
+                    <code className="highlighter-rouge">Name</code>
+                  </p>
+                </td>
+                <td>
+                  <span className="h6">Devanshu Bisht</span>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <p>
+                    <code className="highlighter-rouge">Status</code>
+                  </p>
+                </td>
+                <td>
+                  <span className="h6">Kidney Stone Stage 3</span>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <p>
+                    <code className="highlighter-rouge">Age</code>
+                  </p>
+                </td>
+                <td>
+                  <span className="h6">23</span>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <p>
+                    <code className="highlighter-rouge">Availability</code>
+                  </p>
+                </td>
+                <td>
+                  <span className="h6">9AM to 5PM</span>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </CCardBody>
       </CCard>
     </>
